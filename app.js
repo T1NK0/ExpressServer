4269301d2f7945407e9e29a89b30c2b7fab2eeda
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-01-26 22:27:17 +0100

    can delete, but only on index 0

--- app.js
+++ app.js
@@ -9,20 +9,32 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 let dictators = Array();
 
-app.get('/getDictator', (req, res) => {
+app.get('/getDictator', function(req, res){
   res.send(dictators)
-})
+});
 
 app.post('/createDictator', function (req, res) {
-  const dictator = req.body;
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const birthyear = req.body.birthyear;
+  const deathyear = req.body.deathyear;
+  const description = req.body.description;
+  const dictator = {'firstname':firstname, 'lastname':lastname, 'birthyear':birthyear, 'deathyear':deathyear, 'description':description};
   console.log(dictator);
   dictators.push(dictator);
-})
+});
+
+app.delete('/deleteDictator', function(req, res){
+  const dictatorIndex = req.body.index;
+  //Selects our dictators array, and splice the element of the index. (,1 to indecate we only delete 1 item.)
+  dictators.splice(dictatorIndex,1);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  //writes to our console that the api is listening on port 3000, as we define at the top of our code.
+  console.log(`Dictator API is listening on port ${port}`)
+});
