410d5282ff287de60d68eaca439efe30d075b4d6
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-01-26 13:19:21 +0100

    api

---
+++ app.js
@@ -0,0 +1,30 @@
+//Setup our express application
+const express = require('express');
+//makes app = to our express application
+const app = express();
+//Setup cors
+const cors = require('cors');
+//Creates a port variable with the value of 3000
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!')
+  })
+  
+  app.post('/dictators', function (req, res) {
+      const firstname = req.body.userName;
+      const lastname = req.body.lastname;
+      const birthyear = req.body.birthyear;
+      const deathyear = req.body.deathyear;
+      const description = req.body.description;
+  
+      res.send({'firstname': firstname, 'lastname': lastname})
+    })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
